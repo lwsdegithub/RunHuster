211a5552823f2a23a56d617317ea1385244a5138
lws <1904792432@qq.com>
2017-11-25 17:34:33 +0800

    nothing

--- app/src/main/java/com/example/run/MainActivity.java
+++ app/src/main/java/com/example/run/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         chartFragment=new ChartFragment();
         TabViewChild tabViewChild01=new TabViewChild(R.drawable.personal_information_tab_image_sel,R.drawable.personal_information_tab_image_unsel,"个人信息",personalInformationFragment);
         TabViewChild tabViewChild02=new TabViewChild(R.drawable.run_tab_image_sel,R.drawable.run_tab_image_unsel,"锻炼界面",runFragment);
-        TabViewChild tabViewChild03=new TabViewChild(R.drawable.location_marker,R.drawable.grade_cal_tab_image_unsel,"成绩计算",gradeCalFragment);
+        TabViewChild tabViewChild03=new TabViewChild(R.drawable.grade_cal_tab_image_sel,R.drawable.grade_cal_tab_image_unsel,"成绩计算",gradeCalFragment);
         TabViewChild tabViewChild04=new TabViewChild(R.drawable.location_marker,R.drawable.chart_tab_iamge_unsel,"变化曲线",chartFragment);
         tabViewChildList.add(tabViewChild01);
         tabViewChildList.add(tabViewChild02);
