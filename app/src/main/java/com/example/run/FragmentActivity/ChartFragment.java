072e3e82e6410b53bb4070c64f6e5a3c1b9f6889
lws <1904792432@qq.com>
2017-11-26 00:02:21 +0800

    实现图表

--- app/src/main/java/com/example/run/FragmentActivity/ChartFragment.java
+++ app/src/main/java/com/example/run/FragmentActivity/ChartFragment.java
@@ -32,17 +32,19 @@ public class ChartFragment extends Fragment implements View.OnClickListener {
     private ImageView stateImg2InCF;
     private TextView theLongRunTimeInCF;
     private TextView theLongRunScoresInCF;
-    private LineChartView lineChartView;
     private View mainView;
 
     //一些图表的变量元素声明
+    private LineChartView lineChartView;
     private List<PointValue> pointValueList=new ArrayList<>();
     private List<Line> lineList=new ArrayList<>();
     private LineChartData lineChartData=new LineChartData();
     private Axis axisX=new Axis();//X轴
     private Axis axisYTime=new Axis();//时间Y轴
     private Axis axisYMileage=new Axis();//里程Y轴
-    private List<AxisValue> axisValueList=new ArrayList<>();//设置坐标值
+    private List<AxisValue> axisXValueList=new ArrayList<>();//设置坐标值
+    private List<AxisValue> axisYTimeValueList=new ArrayList<>();//设置时间Y轴坐标值
+    private List<AxisValue> axisYMileageValueList=new ArrayList<>();//设置里程Y轴坐标值
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -66,7 +68,19 @@ public class ChartFragment extends Fragment implements View.OnClickListener {
     private void setUpLineChartView(){
         lineChartView.setInteractive(true);//设置交互性
         lineChartView.setBackgroundColor(Color.TRANSPARENT);//设置背景为透明
+        //设置X轴
+        axisXValueList.add(new AxisValue(0).setLabel("Mon"));
+        axisXValueList.add(new AxisValue(1).setLabel("Tue"));
+        axisXValueList.add(new AxisValue(2).setLabel("Wed"));
+        axisXValueList.add(new AxisValue(3).setLabel("Thu"));
+        axisXValueList.add(new AxisValue(4).setLabel("Fri"));
+        axisXValueList.add(new AxisValue(5).setLabel("Sat"));
+        axisXValueList.add(new AxisValue(6).setLabel("Sun"));
+        axisX.setValues(axisXValueList);
+        lineChartData.setAxisXBottom(axisX);
 
+
+        lineChartView.setLineChartData(lineChartData);
     }
     @Override
     public void onClick(View view) {
